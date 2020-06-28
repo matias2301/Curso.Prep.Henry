b8a47ec48a4280c426cfc70dba5782ea881f26ad
Matias Bonino <matias2301@hotmail.com>
2020-06-28 20:40:05 -0300

    resolucion ejercicios y creacion de archivos con la teoria solicitada

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -8,25 +8,45 @@ function crearUsuario() {
   // {{nombre}} debe ser el nombre definido en cada instancia
   // Devuelve la clase
   // Tu código:
+  function Usuario( opciones ){
+      this.usuario = opciones.usuario;
+      this.nombre = opciones.nombre;
+      this.email = opciones.email;
+      this.password = opciones.password
+  };
 
+  Usuario.prototype.saludar = function( nombre ){
+    return 'Hola, mi nombre es ' + this.nombre;
+  };
+
+  return Usuario;
 }
 
 function agregarMetodoPrototype(Constructor) {
   // Agrega un método al Constructor del `prototype`
   // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
   // Tu código:
+  Constructor.prototype.saludar = function(){
+    return "Hello World!";
+  }
 
 }
 
 function agregarStringInvertida() {
-  // Agrega un método al prototype de String que devuelva la misma cadena de caracteres, pero invertida.
+  // Agrega un método al prototype de String que devuelva
+  // la misma cadena de caracteres, pero invertida.
   // El método debe llamarse "reverse"
   // Ej: 'menem'.reverse() => menem
   // 'toni'.reverse() => 'inot'
   // Pista: Necesitarás usar "this" dentro de "reverse"
-
+  String.prototype.reverse = function(){
+    var reversa = '';
+    for ( let i = this.length - 1 ; i >= 0; i--){
+      reversa += this[i];
+    }
+    return reversa;
+  }
 }
-
 // No modificar nada debajo de esta línea
 // --------------------------------
 
